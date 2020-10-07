3b2eaa72fb66d815ec8fc827c6216f2a1bc67774
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2020-10-07 09:09:47 +0200

    update version

--- docs/classwork__queue__thread.js
+++
@@ -1,7 +0,0 @@
-var classwork__queue__thread =
-[
-    [ "work_queue_thread", "classwork__queue__thread.html#a92106392380a4d05784490d472bbccdd", null ],
-    [ "~work_queue_thread", "classwork__queue__thread.html#a6645f95a5318307332d23a34be883fc2", null ],
-    [ "on_thread", "classwork__queue__thread.html#abd6662f62ba1e80c36bd9df836f6c763", null ],
-    [ "m_parentWorkQueue", "classwork__queue__thread.html#ac5adb4b45da9e98b3b7644d59e766b63", null ]
-];
--- docs/mn__thread_8hpp.js
+++
@@ -1,7 +0,0 @@
-var mn__thread_8hpp =
-[
-    [ "basic_thread", "classbasic__thread.html", "classbasic__thread" ],
-    [ "foreign_thread", "classforeign__thread.html", "classforeign__thread" ],
-    [ "foreign_thread_t", "mn__thread_8hpp.html#a083416583a6f8eb8414ac2ab729e5fea", null ],
-    [ "thread_t", "mn__thread_8hpp.html#a188e0393c136dab96253afea9c1b7b92", null ]
-];
